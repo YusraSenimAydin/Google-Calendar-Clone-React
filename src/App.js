7b14474b1372dbb637b2946bd35ef994d4d388f7
YusraSenimDede <50294199+YusraSenimAydin@users.noreply.github.com>
2022-05-11 15:03:57 +0400

    created components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React , {useState}from 'react';
 import './App.css';
 import { getMonth} from "./util"
+import CalendarHeader from './CalendarHeader'
+import Sidebar from './Sidebar'
+import Month from './Month'
+
 function App() {
+  const [currentMonth , setCurrentMonth] =useState(getMonth())
   return (
-    <div className="App">
-      console.table(getMonth())
-     
-    </div>
+    <React.Fragment>
+      <div className="h-screen flex flex-columns">
+     <CalendarHeader />
+     <div className="flex flex-1">
+       <Sidebar />
+       <Month month={currentMonth} />
+     </div>
+     </div>
+    </React.Fragment>    
   );
 }
 
